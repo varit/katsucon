590c43469c547a7308f47b7f43911a21b4faefbd
Varit Seekhao <varit.seekhao@gmail.com>
2021-03-09 14:52:46 -0500

    complete CRUD for favorites.js

--- client/src/services/favorites.js
+++ client/src/services/favorites.js
@@ -4,3 +4,23 @@ export const getAllFavorites = async () => {
   const resp = await api.get("/favorites");
   return resp.data;
 }
+
+export const getFavorite = async () => {
+  const resp = await api.get(`/favorites/${id}`);
+  return resp.data;
+}
+
+export const postFavorite = async (favoriteData) => {
+  const resp = await api.post("/favorites", { favorite: favoriteData });
+  return resp.data
+}
+
+export const putFavorite = async (id, favoriteData) => {
+  const resp = await api.put(`/favorites/${id}`, { favorite: favoriteData});
+  return resp.data;
+}
+
+export const detroyFavorite = async (id) => {
+  const resp = await api.delete(`/favorites/${id}`);
+  return resp;
+}
