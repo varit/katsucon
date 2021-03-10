fcdd63fe5590dc927f2a951446faa704fef195b8
Varit Seekhao <varit.seekhao@gmail.com>
2021-03-10 15:49:40 -0500

    edit package.json

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NODE_ENV === "production" ? "deployed link here" : "http://localhost:3000";
+const baseURL = process.env.NODE_ENV === "production" ? "https://varit-waterfall-api.herokuapp.com/" : "http://localhost:3000";
 
 const api = axios.create({
   baseURL: baseURL
